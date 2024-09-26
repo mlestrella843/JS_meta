edd916859d8ebb2070e1bd2f382de5611d266de7
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2024-09-25 19:10:57 -0600

    while loop

---
+++ for_loop.js
@@ -0,0 +1,25 @@
+let counter = 0;
+
+for (let i =0; i<5; i++){
+    console.log("Hola my friends!");
+}
+
+for (let i=1; i<5; i++){
+    console.log(i);
+}
+console.log("GO!!!! ");
+
+for (let i=10; i>0; i--){
+    console.log(i);
+}
+console.log("Happy New Year!!!! ");
+
+for(var i = 1; i<= 5; i++ ){
+    console.log(i);
+}
+console.log('Counting completed');
+
+for(var i = 5; i>= 1; i-- ){
+    console.log(i);
+}
+console.log('Counting completed');
---
+++ while_loop.js
@@ -0,0 +1,30 @@
+// let counter = 3;
+
+// while(counter > 0){
+//     console.log(" Im inside while loop yet");
+//     counter--;
+// }
+
+
+var counter = 1;
+
+while (counter < 6){
+    console.log(counter);
+    counter++;
+}
+console.log('Counting completed');
+
+var counter = 5;
+
+while (counter >=  1 ){
+    console.log(counter);
+    counter--;
+}
+console.log('Counting completed');
+
+var year = 2018;
+
+while (year <= 2022){
+    console.log(year);
+    year++;
+}
