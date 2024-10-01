41ac46823b46b6cb8b426175a83f23a6793975b6
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2024-10-01 10:11:48 -0600

    Inheritance

--- class_inheritance_opp.js
+++ class_inheritance_opp.js
@@ -1,3 +1,30 @@
+// class Animal { 
+//     constructor( color, legs ){
+//         this.color = color;
+//         this.legs = legs;
+//     }
+//     getAnimal(){
+//         console.log(this.color, this.legs);
+//     }   
+// }
+// class Dog extends Animal{
+//     constructor(color, legs, sound){
+//         super( color, legs);
+//         this.sound = sound;
+//     }
+//     getSoundDog(){
+//         console.log(`The dog say ${this.sound}`);
+//     }
+// }
+// // Create a new object using the prototype
+// var myDog = Object.create(Dog);
+// myDog.color = 'brown';
+// myDog.legs = 4;
+
+// console.log (myDog)
+
+
+
 class Animal { 
     constructor( color, legs ){
         this.color = color;
@@ -10,10 +37,8 @@ class Animal {
 }
 
 class Dog extends Animal{
-
-    constructor(color, legs, breed, sound){
+    constructor(color, legs, sound){
         super( color, legs);
-        this.breed = breed;
         this.sound = sound;
     }
     getSoundDog(){
@@ -21,7 +46,22 @@ class Dog extends Animal{
     }
 }
 
+class Chihuahua extends Dog{
+    constructor(color, legs, sound, breed){
+        super(color, legs, sound);
+        this.breed = breed;
+    }
+    getBreed(){
+        console.log(`The breed of my dog is ${this.breed}`);
+    }
+
+}
+
 // var myDog = Object.create(Animal);
-var myDog = new Dog("Black", 4, "German", "uwffff");
+const myDog = new Dog("Black", 4, "woof");
+const chihuahua = new Chihuahua("brown", 4, "woof", "Chihuahua");
+
 myDog.getSoundDog();
+chihuahua.getBreed();
+console.log(chihuahua);
 console.log (myDog);
