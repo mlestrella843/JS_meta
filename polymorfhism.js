8d956a4003ba194a2ab15c4eb7109dde4c58844c
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2024-10-02 09:22:03 -0600

    Polymorfhism

--- polymorfhism.js
+++ polymorfhism.js
@@ -11,3 +11,36 @@ const door = {
 
 bicycle.bell(); // "Get away, please"
 door.bell(); // "Come here, please"
+
+//********************* Another example */
+
+// create your classes hereclass Bird {
+    class Bird {
+        useWings() {
+            console.log("Flying!")
+        }
+    }
+    class Eagle extends Bird {
+        useWings() {
+            super.useWings()
+            console.log("Barely flapping!")
+        }
+    }
+    class Penguin extends Bird {
+        useWings() {
+            console.log("Diving!")
+        }
+    }
+    
+    class Parrot extends Bird {
+        useWings() {
+            console.log("I have a super open and big eyes");
+        }
+    }
+    
+    var baldEagle = new Eagle();
+    var kingPenguin = new Penguin();
+    var owl1 = new Parrot();
+    owl1.useWings();
+    baldEagle.useWings(); // "Flying! Barely flapping!"
+    kingPenguin.useWings(); // "Diving!"
