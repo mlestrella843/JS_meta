d86b37176d442675e2640cd2e1e23d7898504c8e
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2024-10-08 22:33:54 -0600

    restructuring

--- JS_1/rest_operator.js
+++ JS_1/rest_operator.js
@@ -1,6 +1,28 @@
 //1. Rest Operator in Function Parameters
+/* 
+Breakdown:
+The initial value for acc is 1 (instead of 0), so the accumulation starts with 1.
+
+First iteration:
+
+acc starts at 1 (the initial value).
+curr is the first element of the array, 1.
+acc + curr becomes 1 + 1 = 2.
+acc is now updated to 2.
+Second iteration:
+
+curr is now the second element, 2.
+acc + curr becomes 2 + 2 = 4.
+acc is now updated to 4.
+Third iteration:
+
+curr is now the third element, 3.
+acc + curr becomes 4 + 3 = 7.
+acc is now updated to 7.
+
+*/
 function sumAll(...numbers) {
-    return numbers.reduce((acc, curr) => acc + curr, 0);
+    return numbers.reduce((acc, curr) => acc + curr, 1);
 }
 
 console.log(sumAll(1, 2, 3));        // Output: 6
@@ -32,3 +54,7 @@ const { name, ...details } = person;
 console.log(name);    // Output: "John"
 console.log(details); // Output: { age: 30, occupation: 'Developer', country: 'USA' }
 
+
+const meal = ["soup", "steak", "ice cream"]
+let [starter] = meal;
+console.log(starter);
