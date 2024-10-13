6fbba9331b78a36aa96e67885662dd81afe527f2
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2024-10-12 22:22:20 -0600

    Objects iteration

---
+++ ReviewObjects/objects.js
@@ -0,0 +1,48 @@
+
+const arrayObjects = [ 
+
+{
+    name: "Maria",
+    lastName: "Star"
+},
+
+{
+    name: "John",
+    lastName: "Doe"
+}
+
+]
+const printNames = (arrayObjects) => {
+    arrayObjects.forEach(person => {
+        console.log(`${person.name} ${person.lastName}`);
+    })
+}
+
+printNames(arrayObjects);
+
+console.log("\n");
+
+// objects, using Object.keys
+
+const persona1 = {
+    name: "Julian",
+    lastName: "Alvarez",
+    age: 30
+}
+
+Object.keys(persona1).forEach((key) => {
+    console.log(`${key}: ${persona1[key]}`);
+})
+
+console.log("\n");
+//Objects, using for...in
+
+const persona2 = {
+    name: "Nicolas",
+    lastName: "Garcia",
+    age: 28
+}
+
+for (const key in persona2){
+    console.log(`${key}: ${persona2[key]}`);
+}
